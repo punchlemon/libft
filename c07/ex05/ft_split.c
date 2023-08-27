82a3710cd64bfc6b15b0ef936d7fcf1fd368c5f8
punchlemon <tsukiwahigashini0410@gmail.com>
2023-08-28 06:28:21 +0900

    change c07 ex05

--- c07/ex05/ft_split.c
+++ c07/ex05/ft_split.c
@@ -12,114 +12,105 @@
 
 #include <stdlib.h>
 
-int	ft_strlen(char *str)
-{
-	int	i;
-
-	i = 0;
-	while (str[i] != 0)
-		i++;
-	return (i);
-}
-
-int	count_words(char *str, char *sep)
-{
-	int	i;
-	int	word_count;
-	int	flag;
-
-	flag = 0;
-	i = 0;
-	while (str[i])
-	{
-		if (check_charset(str[i], sep))
-			flag = 0;
-		else
-		{
-			if (flag == 0)
-			{
-				flag = 1;
-				word_count++;
-			}
-		}
-		i++;
-	}
-	return (word_count);
-}
-
-int	store_str(char *str, char *sep)
-{
-	static int	d;
-	int			i;
-	int			char_count;
-	int			flag;
-	char		*result;
-
-	flag = 0;
-	char_count = 0;
-	i = 0;
-	while (str[i])
-	{
-		if (check_charset(str[i], sep))
-			if (flag == 1)
-				break ;
-		else
-		{
-			flag = 1;
-			char_count++;
-		}
-	}
-	i = 0;
-	result = (char *)malloc(sizeof(char) * char_count + 1);
-	if (result != NULL)
-	{
-		while (str[i])
-		{
-			if (check_charset(str[i], sep))
-				if (flag == 1)
-					break ;
-			else
-			{
-				if (flag == 0)
-				{
-					flag = 1;
-					str[i] = i;
-				}
-			}
-			i++;
-		}
-	}
-}
-
 int	check_charset(char c, char *charset)
 {
-	int	len;
 	int	i;
 
-	len = ftstrlen(charset);
-	i = 0;
-	while (i < len)
+	i = -1;
+	while (charset[++i])
 		if (c == charset[i])
 			return (1);
 	return (0);
 }
 
-char	**ft_split(char *str, char *charset)
+void	make_str(char *str, char *sep, char **result)
 {
-	int		str_len;
-	int		word_count;
-	char	**result;
 	int		i;
+	int		d;
+	int		char_count;
 
-	str_len = ft_strlen(str);
-	word_count = count_words(str, charset);
-	result = (char **)malloc(sizeof(char *) * word_count + 1);
-	result[word_count] = NULL;
+	d = 0;
 	i = 0;
-	while (i < word_count)
+	while (result[i])
 	{
-		result[i] = store_str(str, charset);
+		char_count = 0;
+		while (check_charset(str[d], sep))
+			d++;
+		while (!check_charset(str[d], sep))
+		{
+			char_count++;
+			d++;
+		}
+		result[i] = (char *)malloc(sizeof(char) * (char_count + 1));
+		result[i][char_count] = '\0';
 		i++;
 	}
+}
 
+void	store_str(char *str, char *sep, char **result)
+{
+	int		i;
+	int		d;
+	int		char_count;
+
+	d = 0;
+	i = 0;
+	while (result[i])
+	{
+		char_count = 0;
+		while (check_charset(str[d], sep))
+			d++;
+		while (!check_charset(str[d], sep))
+		{
+			result[i][char_count] = str[d];
+			char_count++;
+			d++;
+		}
+		result[i][char_count] = '\0';
+		i++;
+	}
+}
+
+char	**make_strs(char *str, char *sep)
+{
+	int		i;
+	int		word_count;
+	int		flag;
+	char	**result;
+
+	word_count = 0;
+	flag = 0;
+	i = -1;
+	while (str[++i])
+	{
+		if (check_charset(str[i], sep))
+			flag = 0;
+		else if (flag == 0)
+		{
+			flag = 1;
+			word_count++;
+		}
+	}
+	result = (char **)malloc(sizeof(char *) * (word_count + 1));
+	if (result == NULL)
+		return (NULL);
+	result[word_count] = NULL;
+	return (result);
+}
+
+char	**ft_split(char *str, char *charset)
+{
+	char	**result;
+
+	result = make_strs(str, charset);
+	make_str(str, charset, result);
+	store_str(str, charset, result);
+	return (result);
+}
+
+#include <stdio.h>
+int main(){
+	char **r = ft_split("hef;jei.af;f.ejaio;ejfae;gal", ",;");
+	for (int i = 0; r[i]; i++)
+		printf("%s\n", r[i]);
 }
