bde929c0b1e7ea00083d1e22e6e8a4cd1751438e
punchlemon <tsukiwahigashini0410@gmail.com>
2024-05-30 10:31:02 +0900

    Edit to path norminette

--- ft_lstadd_front.c
+++ ft_lstadd_front.c
@@ -6,7 +6,7 @@
 /*   By: retanaka <retanaka@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 10:37:49 by retanaka          #+#    #+#             */
-/*   Updated: 2024/04/23 06:46:37 by retanaka         ###   ########.fr       */
+/*   Updated: 2024/05/30 10:26:17 by retanaka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,5 @@ void	ft_lstadd_front(t_list **lst, t_list *new)
 {
 	if (lst != NULL || new != NULL)
 		new -> next = *lst;
-		*lst = new;
+	*lst = new;
 }
