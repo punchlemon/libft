bf32b37fc0cc7ab09eae9044ec37a7efb7d7b915
punchlemon <tsukiwahigashini0410@gmail.com>
2024-06-08 00:10:43 +0900

    blush up ft_memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -15,17 +15,13 @@
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
 	size_t	i;
-	char	*d;
-	char	*s;
 
 	if (dst == NULL && src == NULL)
 		return (NULL);
-	d = (char *)dst;
-	s = (char *)src;
 	i = 0;
 	while (i < n)
 	{
-		d[i] = s[i];
+		*(char *)(dst + i) = *(char *)(src + i);
 		i++;
 	}
 	return (dst);
